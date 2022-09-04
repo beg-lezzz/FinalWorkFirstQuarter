687019f5b87660902fde4872101d4778b146cb8c
malistov.artem <malistov.artem@russianpost.ru>
2022-09-04 16:09:42 +0300

    add comments

--- FinalWorkFirstQuarter/Program.cs
+++ FinalWorkFirstQuarter/Program.cs
@@ -1,9 +1,10 @@
-﻿//метод запроса информации у пользователя (число)
+﻿// метод запроса информации у пользователя (число)
 int UserNumData(string inputString)
 {
     Console.Write(inputString + ": ");
     string numString = Console.ReadLine();
-    if (Int32.TryParse(numString, out int num) == true && num > 0)                                       //проверяем, что введено положительное число для строк
+    // проверяем, что введено положительное число для строк
+    if (Int32.TryParse(numString, out int num) == true && num > 0)
     {
         return num;
     }
@@ -15,11 +16,12 @@ int UserNumData(string inputString)
     }
 }
 
-//метод запроса информации у пользователя (непустая строка)
+// метод запроса информации у пользователя (непустая строка)
 string UserStringData(string inputString)
 {
     Console.Write(inputString + ": ");
     string userString = Console.ReadLine();
+    // проверяем, что строка не пуста и туда введен символ
     if (userString == String.Empty)
     {
         Console.WriteLine("Ошибка ввода");
@@ -29,11 +31,11 @@ string UserStringData(string inputString)
     return userString;
 }
 
-//Метод для заполнения одномерного массива с клавиатуры
+// Метод для заполнения одномерного массива с клавиатуры
 string[] FillArray(int arrLength)
 {
     string[] stringArray = new string[arrLength];
-
+// запускаем цикл для запроса у пользователя такого количества элементов, которое он ввел ранее
     for (int i = 0; i < arrLength; i++)
     {
         stringArray[i] = UserStringData($"Введите {i + 1}-й элемент массива");
@@ -42,11 +44,12 @@ string[] FillArray(int arrLength)
     return stringArray;
 }
 
-//метод для создания нового масиива 3-х значных элементов исходного массива
+// метод для создания нового масиива 3-х значных элементов исходного массива
 string[] FillThreeDigitsArray(string[] stringArray)
 {
     int counter = 0;
-    string[] outArray = new string[stringArray.Length];
+    string[] outArray = new string[stringArray.Length]; 
+    // запускаем цикл для перебора всех элементов массива, определения тех, у которых длина меньше или равна 3 и записи их в новый массив
     for (int i = 0; i < stringArray.Length; i++)
     {
         if (stringArray[i].Length < 4)
@@ -55,17 +58,20 @@ string[] FillThreeDigitsArray(string[] stringArray)
             counter++;
         }
     }
+    // меняем длину массива до количества найденных в основном массиве элементов, длина которых меньше, либо равна 3
     Array.Resize(ref outArray, counter);
     return outArray;
 }
 
-//Метод для печати одномерного массива
+// Метод для печати одномерного массива
 void PrintArray(string[] stringArray)
 {
+    // запускаем цикл для последовательного вывода всех элементов входного массива до предпоследнего через запятую
     for (int i = 0; i < stringArray.Length - 1; i++)
     {
         Console.Write(stringArray[i] + ", ");
     }
+    // вывод последнего элемента массива без запятой
     Console.WriteLine(stringArray[stringArray.Length - 1]);
 }
 
@@ -73,11 +79,14 @@ void PrintArray(string[] stringArray)
 void MainMethod()
 {
     Console.Clear();
+    // заполняем массив
     string[] outArray = FillArray(UserNumData("Задайте число элементов массива"));
     Console.Clear();
+    // выводим первоначальный массив
     Console.WriteLine("Первоначальный массив: ");
     PrintArray(outArray);
     Console.WriteLine();
+    // вывод массива с элементами, у которых длина меньше или равна 3
     Console.WriteLine("Массив с элементами 3 и менее символов: ");
     PrintArray(FillThreeDigitsArray(outArray));
 }
