49e444deca1c72f6f7270e43703466935a875c6d
malistov.artem <malistov.artem@russianpost.ru>
2022-09-04 15:48:08 +0300

    add user data query method

--- FinalWorkFirstQuarter/Program.cs
+++ FinalWorkFirstQuarter/Program.cs
@@ -1,1 +1,18 @@
 ﻿Console.Clear();
+
+//метод запроса информации у пользователя (число)
+int UserNumData(string inputString)
+{
+    Console.Write(inputString + ": ");
+    string numString = Console.ReadLine();
+    if (Int32.TryParse(numString, out int num) == true && num > 0)                                       //проверяем, что введено положительное число для строк
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Ошибка ввода");
+        System.Environment.Exit(1);
+        return 0;
+    }
+}
